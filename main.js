f047dbd54a2fd41b9124a147625eb1773e4a96f4
Vironika Lishtvan <Vironika Lishtvan>
2016-10-03 17:15:43 -0400

    the program works, just need to work on scc

---
+++ main.js
@@ -0,0 +1,29 @@
+var time = [
+  ' days', ' years', ' months'];
+var wish = [];//holds wishes inputes
+var text = "";
+
+//function on click
+document.getElementById('submit').onclick = function () {
+
+
+      var myForm = document.getElementById("myForm");
+
+
+     //Adds each wish item to my wish array
+            for(i = 0; i<5; i++){
+               wish.push(myForm.elements[i].value);
+
+            }
+    //loop the massege to repit 5 times
+     for(i=0; i<wish.length; i++){
+         //creats rundom number from 2 to 12
+      var num = Math.floor((Math.random() * 11) + 2);
+         var timeArr = time[Math.floor(Math.random() * time.length)];
+           text +=  "Will get " + wish[i] +" in " + num + timeArr + "<br>";
+         }
+    document.getElementById('message').innerHTML =text;
+
+
+};
+
